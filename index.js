6284183d6624038d386a7ca80ea5ebe35b20f5fd
xaks3636-blip <xaks3636@gmail.com>
2026-02-10 14:07:10 +0300

    Update index.js

--- index.js
+++ index.js
@@ -1,16 +1,9 @@
-
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import App from './App';
+import React from '';
+import ReactDOM from '';
+import App from './App.tsx';
 
 const rootElement = document.getElementById('root');
-if (!rootElement) {
-  throw new Error("Could not find root element to mount to");
-}
-
+if (rootElement) {
 const root = ReactDOM.createRoot(rootElement);
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
+root.render(React.createElement(React.StrictMode, null, React.createElement(App)));
+}
